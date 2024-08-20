ae7ff0af9d659cd64618b263d405cc7ce2526379
David <atembarry12@gmail.com>
2024-08-20 21:22:45 +0100

    PDF Download and Upload

--- src/components/SecureFiles.tsx
+++ src/components/SecureFiles.tsx
@@ -4,11 +4,13 @@ import { userID } from "../redux/idState"
 import supaClient from "../supabase/supaconfig"
 import { v4 as uuidv4 } from 'uuid'
 import { SideNav } from "./SideNav"
+import { TopTitleBar } from "./TopTitle"
 
 // Image URL Template : https://rojcbfneomlgqhvbkpul.supabase.co/storage/v1/object/public/jailbucket/4c1825ca-68fb-427d-8d44-1c8568aa94fb/6056a6a2-e30f-4423-9b7b-42432231bbda
 export default function FileRoute(){
 let user_ID = useAppSelector(userID)
 let [received_file_value, receiveFile] = useState<any[]>([])
+let [received_docs_value, receiveDocs] = useState<any[]>([])
 
 function uploadHandle(e : any)
 {
@@ -17,13 +19,26 @@ function uploadHandle(e : any)
     return eklekto
 }
 
-async function optimus(e : any){
-    let files = uploadHandle(e)
-    console.log(files)
+async function optimus(e : any){async function fetchFiles() {
+    const {data, error} = await supaClient.storage.from('jailbucket').list(user_ID + '/', {
+        limit: 100,
+        offset: 0,
+        sortBy: { column: 'name', order: "asc"}
+    })
+    if(data){
+        let johann =  Array.from(data)
+        let new_array = johann.filter((file) => file.name !== '.emptyFolderPlaceholder')
+        receiveFile(new_array)
+        
+    }
+    else(
+        console.log(error)
+    )
+} 
     let file = uploadHandle(e)[0]
     console.log(file)
 
-    const {data, error} = await supaClient.storage.from('jailbucket').upload(user_ID + '/' + uuidv4(), file)
+    const {data, error} = await supaClient.storage.from('jailbucket').upload(user_ID + '/' + 'documents' +'/' + uuidv4(), file)
     if(data){
         console.log(data)
         fetchFiles()
@@ -49,40 +64,71 @@ async function fetchFiles() {
         console.log(error)
     )
 } 
+async function fetchDocuments() {
+    const {data, error} = await supaClient.storage.from('jailbucket').list(user_ID + '/' +"documents" + "/", {
+        limit: 100,
+        offset: 0,
+        sortBy: { column: 'name', order: "asc"}
+    })
+    if(data){
+        let johann =  Array.from(data)
+        let new_array = johann.filter((file) => file.name !== '.emptyFolderPlaceholder')
+        receiveDocs(new_array)
+        
+    }
+    else(
+        console.log(error)
+    )
+} 
+
 
 useEffect(()=> {
     fetchFiles();
+    fetchDocuments();
 }, [])
 
     return(
         <>
+        <TopTitleBar text="Secure Files"/>
         <div className="body">
             <SideNav/>
             <div>
             <form>
                 <label htmlFor="file_item">Upload an Image</label>
-        <input type='file' id="file_item" onChange={(e)=> {
+        <input type='file' id="image_files" onChange={(e)=> {
             optimus(e)
         }
-            } accept="image/jpg, image/png, image/jpeg"/>
-        <button type='submit'>Upload</button>
+            } accept="image/jpg, image/png, image/jpeg, image/jfif"/>
+        <input type='file' accept=".docx, .pdf" onChange={(e)=> {
+            optimus(e)
+        }}/>
+        <button type='submit'>Upload Image</button>
             </form>
+            <p>Your Images</p>
             <div style={{
-                display: "grid", gridTemplateColumns:"auto auto auto", margin:"1rem 0"
+                display: "grid", gridTemplateColumns:"auto auto auto auto", margin:"1rem 0", gap:'0.5rem '
             }}>
             {received_file_value.map((file) => (
-                <div key={file.id}>
+                <div key={file.id} className="grid" style={{
+                    justifyContent:"center"
+                }}>
                 <img 
                 src={`https://rojcbfneomlgqhvbkpul.supabase.co/storage/v1/object/public/jailbucket/${user_ID}/${file.name}`} 
                 alt={file.name}
                 style={{
-                    maxHeight:'10rem',                    
+                    maxWidth:'10rem',                    
                     borderRadius:'1.5rem'
                 }} /> 
 </div>
             )
         )
     }
+    <p>Your Documents</p>
+    {received_docs_value.map((doc) =>(
+        <a download href={`https://rojcbfneomlgqhvbkpul.supabase.co/storage/v1/object/public/jailbucket/${user_ID}/documents/${doc.name}`}>
+            {doc.name}
+        </a>
+    ) )}
         </div>
         </div>
         </div>
